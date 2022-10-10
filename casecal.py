999e388680e704d8f55c176b6ecf696ff8b01d93
Allen Carl <allencarldelasalas@gmail.com>
2022-10-10 18:11:08 +0800

    casecalulator

---
+++ casecal.py
@@ -0,0 +1,15 @@
+
+cal = input("Enter an operation:")
+
+match cal:
+    case "+":
+        a= int(input("Enter your first number:"))
+        
+        b= int(input("Enter your second number:"))
+
+        equal = a + b
+
+        print("Is equal to:", equal)
+
+
+        
